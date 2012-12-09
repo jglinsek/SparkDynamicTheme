6ffa8762228c94402f62b7a876f59c66de254ecc
Ben Hyrman <ben.hyrman@gmail.com>
2012-12-08 19:00:53 -0500

    basic spark structure + SM support

---
+++ SparkDynamicTheme/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SparkDynamicTheme
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
