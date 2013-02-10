a8f8ba1881d6e1eea2c2f32290927215a26d2130
James Glinsek <james.glinsek@rezstream.com>
2013-02-10 15:25:48 -0600

    Added a couple levels of partials.

---
+++ SparkDynamicTheme/Models/DataModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SparkDynamicTheme.Models
+{
+	public class SomeDataModel
+	{
+	}
+
+	public class MoreDataModel
+	{
+	}
+
+	public class EvenMoreDataModel
+	{
+	}
+}
